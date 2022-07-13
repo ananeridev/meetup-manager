6959b6b07f9d87d0910d8ef6e5d7b81501f864c9
anabneri <neribia7@gmail.com>
2022-07-13 11:34:03 -0300

    initial commit
    Signed-off-by: anabneri <neribia7@gmail.com>

---
+++ meetupmanager/src/main/java/com/demo/meetupmanager/MeetupmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.demo.meetupmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetupmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetupmanagerApplication.class, args);
+	}
+
+}
---
+++ meetupmanager/src/test/java/com/demo/meetupmanager/MeetupmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.meetupmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetupmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
